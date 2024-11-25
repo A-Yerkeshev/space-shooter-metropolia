3760733b9604e136bbf7ba1075d2ff8ac833bb24
Arman Yerkesh <a.yerkesh@gmail.com>
2024-11-25 14:58:21 +0200

    Migrate from macOS project to iOS project

--- SpaceShooter/ContentView.swift
+++ SpaceShooter/ContentView.swift
@@ -7,55 +7,79 @@
 
 import SwiftUI
 import SwiftData
+import SpriteKit
+import GameKit
+
+class GameScene: SKScene {
+    let background = SKSpriteNode(imageNamed: "stars-background")
+    var player = SKSpriteNode()
+    var playerFire = SKSpriteNode()
+    
+    var fireTimer = Timer()
+    
+    override func didMove(to view: SKView) {
+        scene?.size = CGSize(width: 750, height: 1335)
+        background.position = CGPoint(x: size.width / 2, y: size.height / 2)
+        background.setScale(2.1)
+        background.zPosition = 1
+        addChild(background)
+        
+        makePlayer(playerCh: 1)
+        
+        fireTimer = .scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(playerFireFunction), userInfo: nil, repeats: true)
+    }
+    
+    func makePlayer(playerCh: Int) {
+        var shipName = ""
+        
+        switch playerCh {
+        case 1:
+            shipName = "ship_1"
+        case 2:
+            shipName = "ship_5"
+        default:
+            shipName = "ship_6"
+        }
+        
+        player = .init(imageNamed: shipName)
+        player.position = CGPoint(x: size.width / 2, y: 120)
+        player.zPosition = 10
+        player.setScale(2.8)
+        addChild(player)
+    }
+    
+    @objc func playerFireFunction() {
+        playerFire = .init(imageNamed: "shot")
+        playerFire.position = player.position
+        playerFire.zPosition = 3
+        
+        addChild(playerFire)
+        
+        let moveAction = SKAction.moveTo(y: 1400, duration: 1)
+        let deleteAction = SKAction.removeFromParent()
+        let combine = SKAction.sequence([moveAction, deleteAction])
+        
+        playerFire.run(combine)
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let location = touch.location(in: self)
+            
+            player.position.x = location.x
+        }
+    }
+}
 
 struct ContentView: View {
-    @Environment(\.modelContext) private var modelContext
-    @Query private var items: [Item]
+    let scene = GameScene()
 
     var body: some View {
-        NavigationSplitView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
-                    } label: {
-                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
-                    }
-                }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-        } detail: {
-            Text("Select an item")
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(timestamp: Date())
-            modelContext.insert(newItem)
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                modelContext.delete(items[index])
-            }
-        }
+        SpriteView(scene: scene)
+            .ignoresSafeArea()
     }
 }
 
 #Preview {
     ContentView()
-        .modelContainer(for: Item.self, inMemory: true)
 }
