f3ea69e96b33ee2bd26cef14ba4544e71e310aa3
Arman Yerkesh <a.yerkesh@gmail.com>
2024-11-25 14:53:43 +0200

    Initial Commit

---
+++ SpaceShooterTests/SpaceShooterTests.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceShooterTests.swift
+//  SpaceShooterTests
+//
+//  Created by Arman Yerkeshev on 25.11.2024.
+//
+
+import Testing
+@testable import SpaceShooter
+
+struct SpaceShooterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
