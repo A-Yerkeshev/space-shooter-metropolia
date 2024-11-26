ba762eb66aeed4cf4678dd0f2c2c6cd611f1e97c
Artur Gol <artur.golavskiy@metropolia.fi>
2024-11-26 15:03:51 +0200

    added more physics body

--- SpaceShooter/ContentView.swift
+++ SpaceShooter/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftData
 import SpriteKit
 import GameKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     let background = SKSpriteNode(imageNamed: "stars-background")
     var player = SKSpriteNode()
     var playerFire = SKSpriteNode()
@@ -19,7 +19,16 @@ class GameScene: SKScene {
     var fireTimer = Timer()
     var enemyTimer = Timer()
     
+    struct CBitmask {
+        static let playerShip: UInt32 = 0b1 //1
+        static let playerFire: UInt32 = 0b10 //2
+        static let enemyShip: UInt32 = 0b100 //4
+        static let bossOne: UInt32 = 0b1000 //8
+    }
+    
     override func didMove(to view: SKView) {
+        physicsWorld.contactDelegate = self
+        
         scene?.size = CGSize(width: 750, height: 1335)
         background.position = CGPoint(x: size.width / 2, y: size.height / 2)
         background.setScale(2.1)
@@ -31,6 +40,33 @@ class GameScene: SKScene {
         fireTimer = .scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(playerFireFunction), userInfo: nil, repeats: true)
         
         enemyTimer = .scheduledTimer(timeInterval: 1, target: self, selector: #selector(makeEnemy), userInfo: nil, repeats: true)
+    }
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        let contactA : SKPhysicsBody
+        let contactB : SKPhysicsBody
+        
+        if contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask {
+            contactA = contact.bodyA
+            contactB = contact.bodyB
+        } else {
+            contactA = contact.bodyB
+            contactB = contact.bodyA
+        }
+        
+        if contactA.categoryBitMask == CBitmask.playerShip && contactB.categoryBitMask == CBitmask.enemyShip {
+            
+            enemy.removeFromParent()
+            playerFire.removeFromParent()
+            
+        }
+        
+        if contactA.categoryBitMask == CBitmask.playerShip && contactB.categoryBitMask == CBitmask.enemyShip {
+            
+            enemy.removeFromParent()
+            player.removeFromParent()
+            
+        }
     }
     
     func makePlayer(playerCh: Int) {
@@ -49,6 +85,12 @@ class GameScene: SKScene {
         player.position = CGPoint(x: size.width / 2, y: 120)
         player.zPosition = 10
         player.setScale(2.8)
+        player.physicsBody = SKPhysicsBody(rectangleOf: player.size)
+        player.physicsBody?.affectedByGravity = false
+        player.physicsBody?.isDynamic = true
+        player.physicsBody?.categoryBitMask = CBitmask.playerShip
+        player.physicsBody?.contactTestBitMask = CBitmask.enemyShip
+        player.physicsBody?.collisionBitMask = CBitmask.enemyShip
         addChild(player)
     }
     
@@ -56,6 +98,11 @@ class GameScene: SKScene {
         playerFire = .init(imageNamed: "shot")
         playerFire.position = player.position
         playerFire.zPosition = 3
+        playerFire.physicsBody = SKPhysicsBody(rectangleOf: playerFire.size)
+        playerFire.physicsBody?.affectedByGravity = false
+        playerFire.physicsBody?.categoryBitMask = CBitmask.playerShip
+        playerFire.physicsBody?.contactTestBitMask = CBitmask.enemyShip
+        playerFire.physicsBody?.collisionBitMask = CBitmask.enemyShip
         
         addChild(playerFire)
         
@@ -74,7 +121,11 @@ class GameScene: SKScene {
         enemy.zPosition = 5
         enemy.setScale(2.8)
         enemy.zRotation = 3.14159
-
+        enemy.physicsBody = SKPhysicsBody(rectangleOf: enemy.size)
+        enemy.physicsBody?.affectedByGravity = false
+        enemy.physicsBody?.categoryBitMask = CBitmask.enemyShip
+        enemy.physicsBody?.contactTestBitMask = CBitmask.playerShip | CBitmask.playerFire
+        enemy.physicsBody?.collisionBitMask = CBitmask.playerShip | CBitmask.playerFire
         addChild(enemy)
         
         let moveAction = SKAction.moveTo(y: -100, duration: 3)
