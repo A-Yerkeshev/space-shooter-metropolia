a14775eaf46a045825c5fd2c6c8d549f007b73f7
LVNDLORD <andydeshko@gmail.com>
2024-12-11 16:57:13 +0200

    some styling for Leaderboard view

--- SpaceShooter/LeaderboardView.swift
+++ SpaceShooter/LeaderboardView.swift
@@ -10,39 +10,41 @@ import SwiftData
 struct LeaderboardView: View {
     @Environment(\.presentationMode) var presentationMode
     @Query(sort: \BestScore.score, order: .reverse) var bestScores: [BestScore]
-
+    
     var body: some View {
         ZStack {
             Image("spaceShooterMenu")
                 .resizable()
                 .scaledToFill()
                 .ignoresSafeArea()
-
+                .overlay(
+                    Color.black.opacity(0.4)
+                )
+            
             VStack {
                 HStack {
-                                    Button(action: {
-                                        presentationMode.wrappedValue.dismiss()
-                                    }) {
-                                        HStack {
-                                            Image(systemName: "chevron.left")
-                                                .font(.system(size: 20, weight: .bold))
-                                                .foregroundColor(.white)
-                                            Text("Back")
-                                                .font(.custom("Chalkduster", size: 30))
-                                                .foregroundColor(.white)
-                                        }
-                                    }
-                                    Spacer()
-                                }
-                                .padding(.horizontal)
-                                .padding(.top, 50)
-                                
-                                Text("Leaderboard")
-                                    .font(.custom("Chalkduster", size: 45))
-                                    .fontWeight(.bold)
-                                    .foregroundColor(Color(red: 1.0, green: 0.0, blue: 0.0))
-                                    .padding(.top, 20)
-
+                    Button(action: {
+                        presentationMode.wrappedValue.dismiss()
+                    }) {
+                        HStack {
+                            Image(systemName: "chevron.left")
+                                .font(.system(size: 20, weight: .bold))
+                                .foregroundColor(.white)
+                            Text("Back")
+                                .font(.custom("Chalkduster", size: 30))
+                                .foregroundColor(.white)
+                        }
+                    }
+                    Spacer()
+                }
+                .padding(EdgeInsets(top: 20, leading: 25, bottom: 0, trailing: 0))
+                
+                Text("Leaderboard")
+                    .font(.custom("Chalkduster", size: 45))
+                    .fontWeight(.bold)
+                    .foregroundColor(Color(red: 1.0, green: 0.0, blue: 0.0))
+                    .padding(.top, 10)
+                
                 List(bestScores) { score in
                     HStack {
                         Text(score.name)
